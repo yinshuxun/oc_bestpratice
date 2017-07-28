f3d7d530141781761bd56e96567c45d7418267cf
yinshuxun <shuxun.yin@gmail.com>
2017-07-28 17:16:36 +0800

    first blood

---
+++ oc_bestpratice/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  oc_bestpratice
+//
+//  Created by 印书勋 on 2017/7/28.
+//  Copyright © 2017年 印书勋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        NSString *helloOC = @"Hello Ojbect-c";
+        NSLog(@"my string is %@",helloOC);
+    }
+    return 0;
+}
